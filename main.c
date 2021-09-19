af6fe7c97877b591ee5124f9a0413999b829d3b8
imsamuka <imsamuka@gmail.com>
2021-09-19 00:07:18 -0300

    create drawLoose(), drawMan() and askDifficulty()

--- main.c
+++ main.c
@@ -1,6 +1,124 @@
 #include "screenlib.h"
 
+const char BLANK_CHR = ' ';
+
+enum Options { CLOSE, EASY, MEDIUM, HARD };
+const u_char OPT_OFFSET = 48;
+
+void drawMan(char screen[SCREEN_HEIGHT][SCREEN_WIDTH], char countdown)
+{
+  // Clear
+  for (char i = 3; i <= 6; i++)
+      for (char j = 7; j <= 9; j++)
+        screen[i][j] = BLANK_CHR;
+
+  switch (countdown)
+  {
+  case 0:
+    screen[6][9] = '\\';
+  case 1:
+    screen[6][7] = '/';
+  case 2:
+    screen[4][9] = '\\';
+  case 3:
+    screen[4][7] = '/';
+  case 4:
+    screen[4][8] = '|';
+    screen[5][8] = '|';
+  case 5:
+    screen[3][8] = 'O';
+  default:
+    break;
+  }
+}
+
+void drawLoose(char screen[SCREEN_HEIGHT][SCREEN_WIDTH])
+{
+  // Pole
+  for (char i = 2; i <= 8; i++)
+    screen[i][2] = '|';
+
+  // Line above
+  insertStr(screen, "_______", 1, 2);
+
+  // Above the head
+  screen[2][8] = '|';
+}
+
+enum Options askDifficulty(char screen[SCREEN_HEIGHT][SCREEN_WIDTH], enum Options opt)
+{
+
+  // Clear screen
+  clear();
+  fillScr(screen, BLANK_CHR);
+
+  insertStr(screen, "C Hangman!", 1, 6);
+  insertStr(screen, "Select a Difficulty:", 3, 3);
+
+  insertStr(screen, "Hard", 5, 6);
+  insertStr(screen, "Medium", 6, 6);
+  insertStr(screen, "Easy", 7, 6);
+  insertStr(screen, "Close", 8, 6);
+
+  screen[5][4] = HARD + OPT_OFFSET;
+  screen[6][4] = MEDIUM + OPT_OFFSET;
+  screen[7][4] = EASY + OPT_OFFSET;
+  screen[8][4] = CLOSE + OPT_OFFSET;
+
+  insertStr(screen, "(default)", 8 - opt, 14);
+
+  renderScr(screen);
+
+  {
+    u_char selection;
+
+    printf("Type the number: ");
+    scanf("%c", &selection);
+    if (selection - OPT_OFFSET >= CLOSE && selection - OPT_OFFSET <= HARD)
+    {
+      opt = selection - OPT_OFFSET;
+    }
+  }
+
+
+  printf("Selected ");
+
+  switch (opt)
+  {
+  case HARD:
+    printf("Hard!!!!");
+    break;
+  case MEDIUM:
+    printf("Medium!");
+    break;
+  case EASY:
+    printf("Easy...");
+    break;
+  case CLOSE:
+  default:
+    printf("Close :(");
+    break;
+  }
+
+  printf("\n");
+
+  return opt;
+}
+
 int main() {
-  printf("Test\n");
+
+  char screen[SCREEN_HEIGHT][SCREEN_WIDTH];
+  enum Options opt = askDifficulty(screen, HARD);
+
+  while (opt != CLOSE)
+  {
+    // Select a word
+    // Start the game
+    // Start Again
+    opt = askDifficulty(screen, opt);
+  }
+
+  printf("Goodbye! Let's play again :>\n");
+
   return 0;
 }

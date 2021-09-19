dfc0c7560f365d2e533eb368f595c6ce3e532d0d
imsamuka <imsamuka@gmail.com>
2021-09-18 23:58:15 -0300

    improve insertStr and renderScr functions

--- screenlib.c
+++ screenlib.c
@@ -8,7 +8,11 @@ void clear()
 
 void insertStr(char screen[SCREEN_HEIGHT][SCREEN_WIDTH], char* str, char y, char x)
 {
+  u_char i, chr;
+  for (i = 0; str[i] != '\0'; i++);
+  chr = screen[y][x + i];
   strcpy(&screen[y][x], str);
+  screen[y][x + i] = chr; // Replace the '\0' character by the previous character
 }
 
 void fillScr(char screen[SCREEN_HEIGHT][SCREEN_WIDTH], char chr)
@@ -23,7 +27,7 @@ void renderScr(char screen[SCREEN_HEIGHT][SCREEN_WIDTH])
   for (char i = 0; i < SCREEN_HEIGHT; i++)
   {
     for (char j = 0; j < SCREEN_WIDTH; j++)
-      printf("%c", screen[i][j]);
-    printf("\n");
+      putchar(screen[i][j]);
+    putchar('\n');
   }
 }
